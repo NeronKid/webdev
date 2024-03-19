8b255089aff5cf6d194fbc97f128b53ae870d5b6
NeronKid <dienikiurus@gmail.com>
2024-03-19 07:01:30 +0300

    working start application

--- accounting/src/main/java/com/serving/accounting/AccountingApplication.java
+++ accounting/src/main/java/com/serving/accounting/AccountingApplication.java
@@ -2,8 +2,9 @@ package com.serving.accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AccountingApplication {
 	  public static void main(String[] args) {
 		    SpringApplication.run(AccountingApplication.class, args);
